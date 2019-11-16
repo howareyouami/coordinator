3db8885b2b97f6d8bff86532b0149753bef6dee4
amal jose <amal.jose@appsintegra.com>
2019-11-16 14:49:38 +0400

    Adding homepage

---
+++ docs/precache-manifest.c172cb4e47f9a043031ac4d59e15ee7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274e6e19383b5378ced0f3facc69479b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8f1dbe9c75555504bc9d",
+    "url": "./static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "64cec1807e112c191113",
+    "url": "./static/js/2.becabee6.chunk.js"
+  },
+  {
+    "revision": "8f1dbe9c75555504bc9d",
+    "url": "./static/js/main.1e9adc18.chunk.js"
+  },
+  {
+    "revision": "dcd526ec2c8ee3b3a454",
+    "url": "./static/js/runtime-main.e48239c1.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.382e1af648b04788943e55884cafe0fb.js"
+  "./precache-manifest.c172cb4e47f9a043031ac4d59e15ee7a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
